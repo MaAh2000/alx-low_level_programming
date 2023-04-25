230bd4948d769779a3dfce9cbe1ba1fc82dccdb8
MaAh2000 <mah49239@gmail.com>
2023-04-25 14:58:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include "stdio.h"
-
+#include "main.h"
 /**
  * main - Printf
  * Return: Always 0
  */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	printf("_putchar\n");
+	return (0);
 }
