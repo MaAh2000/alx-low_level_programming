1f524b170416b3c17f8c9d42ba6ed0aae2dd600f
MaAh2000 <mah49239@gmail.com>
2023-04-25 15:49:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* print_alphabet - function that prints the alphabet.
+*/
+void print_alphabet(void)
+{
+char ch;
+for (ch = "a" ; ch <= "z"; ch++)
+_putchar(ch);
+_putchar('\n');
+}
