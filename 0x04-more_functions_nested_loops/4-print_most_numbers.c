8a2fa51a41d3917cd444bfb71fc0dbc07734c8df
MaAh2000 <mah49239@gmail.com>
2023-04-27 12:42:13 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char h;
+
+	for (h = '0'; h <= '9'; h++)
+	{
+	if (!(h == '2' || h == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
+}
